262cb9f35791c8211009d8c7191e42ce973a190d
CrimsonRiotx <sam_utz@yahoo.com>
2022-12-07 17:16:20 -0500

    Merging changes to Main

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         Scanner input = new Scanner(System.in);
-        System.out.println("Welcome to the Scrabble Score Checker!\nInput the word you would like to play:");
+        System.out.println("Welcome to the Scrabble Score Calculator!\nInput the word you would like to play:");
         String userInput = input.nextLine();
         Scrabble scrabble = new Scrabble(userInput);
-        System.out.println("your score is " + scrabble.getScore());
+        if(scrabble.getScore() > 0) {
+            System.out.println("your score is " + scrabble.getScore());
+        }
+        else{System.out.println("Input not valid. No numbers, symbols, or spaces allowed");
+        }
     }
 }
--- src/ScabbleScore.java
+++ src/ScabbleScore.java
@@ -1,100 +1,67 @@
 class Scrabble {
 
     String word;
+    boolean isValidWord;
+
     Scrabble (String word) {
         this.word = word.toLowerCase();
     }
 
+
     int getScore() {
         int score = 0;
         int ind = 0;
-        int throwaway = 0;
+        boolean isValidWord = true;
         while (ind < word.length()) {
-            //switch statement to count each letter's occurance
+            //switch statement to count/add score for each letter's occurrence
             switch (word.charAt(ind)) {
                 case 'a':
+                case 'e':
+                case 'i':
+                case 'l':
+                case 'n':
+                case 'o':
+                case 'r':
+                case 's':
+                case 't':
+                case 'u':
                     score++;
-                    break;
-                case 'b':
-                    score += 3;
-                    break;
-                case 'c':
-                    score += 3;
                     break;
                 case 'd':
-                    score += 2;
-                    break;
-                case 'e':
-                    score++;
-                    break;
-                case 'f':
-                    score += 4;
-                    break;
                 case 'g':
                     score += 2;
                     break;
+                case 'b':
+                case 'c':
+                case 'm':
+                case 'p':
+                    score += 3;
+                    break;
+
+                case 'f':
                 case 'h':
+                case 'v':
+                case 'w':
+                case 'y':
                     score += 4;
-                    break;
-                case 'i':
-                    score++;
-                    break;
-                case 'j':
-                    score += 8;
                     break;
                 case 'k':
                     score += 5;
                     break;
-                case 'l':
-                    score++;
-                    break;
-                case 'm':
-                    score += 3;
-                    break;
-                case 'n':
-                    score++;
-                    break;
-                case 'o':
-                    score++;
-                    break;
-                case 'p':
-                    score += 3;
-                    break;
-                case 'q':
-                    score += 10;
-                    break;
-                case 'r':
-                    score++;
-                    break;
-                case 's':
-                    score++;
-                    break;
-                case 't':
-                    score++;
-                    break;
-                case 'u':
-                    score++;
-                    break;
-                case 'v':
-                    score += 4;
-                    break;
-                case 'w':
-                    score += 4;
-                    break;
+                case 'j':
                 case 'x':
                     score += 8;
                     break;
-                case 'y':
-                    score += 4;
-                    break;
+                case 'q':
                 case 'z':
                     score += 10;
                     break;
-                default: System.out.println("Invalid character found. No more than one word may be used. Only letters are allowed.");
-                break;
+                default: isValidWord = false; break;
             }
             ind++;
+        }if(isValidWord = true) {
+            return score;
         }
-        return score;
+        else return 0;
     }
 }
